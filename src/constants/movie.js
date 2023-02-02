8c68d9a9eb94fc8e795b73bae272dc28f4786d19
Haziq Yusof <haziq_yusof@hotmail.com>
2023-02-02 15:55:25 -0500

    Update grid styling

--- src/constants/movie.js
+++ src/constants/movie.js
@@ -1,4 +1,4 @@
 import { BACKEND_URL, APIKEY } from "./http"
 
-export const MOVIE_SEARCH_ENDPOINT = `${BACKEND_URL}/SearchMovie/${APIKEY}/`
+export const MOVIE_SEARCH_ENDPOINT = `${BACKEND_URL}/AdvancedSearch/${APIKEY}/`
 export const TOP_250_ENDPOINT = `${BACKEND_URL}/Top250Movies/${APIKEY}/`
