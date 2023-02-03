20a06f0ef96afb10bf23aa0ec80bed7719e96c31
Haziq Yusof <haziq_yusof@hotmail.com>
2023-02-02 22:49:20 -0500

    Update searchbar styling

--- src/components/Core/SearchBar.js
+++ src/components/Core/SearchBar.js
@@ -6,7 +6,7 @@ const SearchBar = ({ placeholder, aria, onSubmit, searchOnTextChange }) => {
 	const didMountRef = useRef(false);
 	const [searchStr, setSearchStr] = useState('')
 	const containerSX = {
-		p: '2px 4px', display: 'flex', alignItems: 'center', width: 600, borderRadius: '30px'
+		p: '2px 4px', display: 'flex', alignItems: 'center', width: 600, borderRadius: '30px', boxShadow: '0 0 10px -3px #000' 
 	}
 
 	// useEffect that triggers when searchStr changes and SearchOnText is true and contains debounce to avoid multiple API calls
